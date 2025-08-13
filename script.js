09cbf027f4a7ad6ec10642484e47d05ab8b83e05
Anmol Aggarwal <Anmolaggarwal1550@gmail.com>
2025-08-13 12:23:49 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Toggle mobile menu
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  if (navMenu.style.display === 'flex') {
+    navMenu.style.display = 'none';
+  } else {
+    navMenu.style.display = 'flex';
+    navMenu.style.flexDirection = 'column';
+  }
+});
